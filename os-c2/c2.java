4d52677b6d623f63a2d1d0d745240afcca2cb6c2
Shawon1997 <16haw87@gmail.com>
2022-06-25 10:06:59 +0530

    it's working

---
+++ os-c2/c2.java
@@ -0,0 +1,19 @@
+Q1 a) What do you know about client server architecture?
+   b)What is the difference between DNS and IP address?
+
+
+Q2. What is CAP theorem, discuss a scenario where CAP theorem helps you
+choose a technology over the other or helps you make a design decision.
+
+
+
+Q3 What are SOLID principles? Explain with examples wherever possible.
+
+
+
+Q4 In the following diagram (it intentionally contains pseudo code) There is an
+interface DataManager with two functions.
+The three classes implement this interface and provide implementation to the
+read and write methods.
+The write method throws an exception in the InMemoryRealmManager class
+because it doesn’t need the write() method
